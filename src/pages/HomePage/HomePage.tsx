c67494676c4ad15174c4cf978bbe3cdb5843abff
Aleksander <sanyok.10rus@gmail.com>
2022-09-08 19:35:18 +0300

    added slider; created grid without content yet

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './HomePage.scss';
+import { CategoriesGrid } from '../../components/CategoriesGrid/CategoriesGrid';
+import { FlexibleSlider } from '../../components/FlexibleSlider/FlexibleSlider';
 
 export const HomePage = () => {
   return (
     <div className="m-width">
       <h1>Home</h1>
+      <FlexibleSlider
+        isHaveDots={true}
+      >
+        <CategoriesGrid />
+        <CategoriesGrid />
+        <CategoriesGrid />
+        <CategoriesGrid />
+      </FlexibleSlider>
     </div>
   );
 };
