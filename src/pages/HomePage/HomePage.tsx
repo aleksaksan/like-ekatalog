8b733533e40f36730cd8e6b408f1f71deda867df
Aleksander <sanyok.10rus@gmail.com>
2022-09-10 05:23:14 +0300

    added centred card component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,6 +5,8 @@ import { FlexibleSlider } from '../../components/FlexibleSlider/FlexibleSlider';
 import { CategoriesGrid4 } from '../../components/CategoriesGrids/CategoriesGrid4';
 import { CategoriesGrid3 } from '../../components/CategoriesGrids/CategoriesGrid3';
 import { CategoriesGrid2 } from '../../components/CategoriesGrids/CategoriesGrid2';
+import { SvgRightArrow } from '../../components/Svg/SvgFuncs/SvgComponents/SvgRightArrow';
+import { CentredItemsCard } from '../../components/CentredItemsCard/CentredItemsCard';
 
 export const HomePage = () => {
   return (
@@ -12,12 +14,17 @@ export const HomePage = () => {
       <h1>Home</h1>
       <FlexibleSlider
         isHaveDots={true}
+        customArrowButtonRight={<SvgRightArrow />}
+        isLooped={true}
+        customDotsButtton={<div className="custom-dot"></div>}
       >
         <CategoriesGrid1 />
         <CategoriesGrid2 />
         <CategoriesGrid3 />
         <CategoriesGrid4 />
       </FlexibleSlider>
+      <h2>Популярное</h2>
+      <CentredItemsCard imageSrc='https://dummyjson.com/image/i/products/2/thumbnail.jpg' name='iphone' price='1000$' />
     </div>
   );
 };
