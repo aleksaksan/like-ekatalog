12f38d1728d6b0789570612292aacc9aa3e3aae1
Aleksander <sanyok.10rus@gmail.com>
2022-10-23 23:37:51 +0400

    fixed name

--- src/hooks/useItemsFilter.ts
+++ src/hooks/useItemsFilter.ts
@@ -2,11 +2,11 @@ import { useMemo } from "react";
 
 //TODO add type to items
 export const useSortedPosts = (items: any, price: number) => {
-    const sortedPosts = useMemo(() => {
+    const sortedItems = useMemo(() => {
     if (price)
       return [...items].sort((a,b) => a[price].localeCompare(b[price]));
     return items; 
   }, [price, items]);
 
-    return sortedPosts;
+    return sortedItems;
 };
